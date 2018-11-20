5f8225b99c9c210c702ea97b3a6efc3fc456d48f
Florian Hoyer <sneezoo.dev@gmail.com>
2018-11-20 21:44:01 +0100

    Project setup

---
+++ app/components/Recipe.vue
@@ -0,0 +1,33 @@
+<template>
+    <Page>
+        <ActionBar :title="recipe.title" class="action-bar" />
+        <ScrollView>
+            <StackLayout>
+                <Label class="c-description" :text="recipe.shortDesc" />
+                <Label class="c-ingredients" text="a lot of text is going to be here" />
+                <Label class="c-cooking" text="even more text will be here" />
+            </StackLayout>
+        </ScrollView>
+    </Page>
+</template>
+
+<script>
+export default {
+    props: {
+        recipe: {
+            type: Object,
+            required: true,
+            default: {
+                id: 0,
+                title: "Ooops",
+                shortDesc: "This should not have happend! Very sorry!",
+                tags: [],
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/components/RecipesList.vue
@@ -0,0 +1,86 @@
+<template>
+    <Page class="page">
+        <ActionBar title="Recipes" class="action-bar" />
+        <ScrollView>
+            <ListView for="item in items" @itemTap="navigateToRecipe">
+                <v-template>
+                    <StackLayout>
+                        <Label :text="item.title" />
+                        <Label :text="item.shortDesc" />
+                    </StackLayout>
+                </v-template>
+            </ListView>
+        </ScrollView>
+    </Page>
+</template>
+
+<script>
+import Recipe from './Recipe';
+export default {
+    data () {
+        return {
+            items: [
+                {
+                    id: 1,
+                    title: "1st meal",
+                    shortDesc: "This is so tasty",
+                    tags: [
+                        "meal",
+                        "some",
+                        "tasty",
+                    ],
+                },
+                {
+                    id: 2,
+                    title: "2nd meal",
+                    shortDesc: "This is so tasty",
+                    tags: [
+                        "meal",
+                        "some",
+                        "tasty",
+                    ],
+                },
+                {
+                    id: 3,
+                    title: "3rd meal",
+                    shortDesc: "This is so tasty",
+                    tags: [
+                        "meal",
+                        "some",
+                        "tasty",
+                    ],
+                },
+                {
+                    id: 4,
+                    title: "4th meal",
+                    shortDesc: "This is so tasty",
+                    tags: [
+                        "meal",
+                        "some",
+                        "tasty",
+                    ],
+                },
+                {
+                    id: 5,
+                    title: "5th meal",
+                    shortDesc: "This is so tasty",
+                    tags: [
+                        "meal",
+                        "some",
+                        "tasty",
+                    ],
+                },
+            ],
+        };
+    },
+    methods: {
+        navigateToRecipe({ item: recipe }) {
+            this.$navigateTo(Recipe, { props: { recipe } });
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
